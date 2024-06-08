ced46ed1267b98bf5bfe7127ee358686ffd7bfee
percegaroli <bruno.percegaroli@gmail.com>
2024-06-08 01:15:31 -0300

    complement loop2DComPlotTwist tests

--- src/ex-22/loop2DComPlotTwist.test.js
+++ src/ex-22/loop2DComPlotTwist.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { loop2D } from './loop2D';
+import { loop2DComPlotTwist } from './loop2DComPlotTwist';
 
-describe('loop2D', () => {
+describe('loop2DComPlotTwist', () => {
     it.each([
         [1, 'A', 'A'],
         [2, 'A', 'A\nA A'],
@@ -9,13 +9,14 @@ describe('loop2D', () => {
         [10, '0', '0\n0 0\n0 0 0\n0 0 0 0\n0 0 0 0 0\n0 0 0 0 0 0\n0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0'],
         [-2, 'X', 'X X\nX'],
         [-1, 'O', 'O']
-    ])('loop2D(%i, %s) => %s', (number, char, result) => {
-        expect(loop2D(number, char)).toBe(result);
+        [-5, 'B', 'B B B B B\nB B B B\nB B B\nB B\nB']
+    ])('loop2DComPlotTwist(%i, %s) => %s', (number, char, result) => {
+        expect(loop2DComPlotTwist(number, char)).toBe(result);
     })
 
     it.each([
         [0, 'B'],
-    ])('loop2D(%i, %s) => undefined', (number, char) => {
-        expect(loop2D(number, char)).toBeUndefined();
+    ])('loop2DComPlotTwist(%i, %s) => undefined', (number, char) => {
+        expect(loop2DComPlotTwist(number, char)).toBeUndefined();
     })
 })
