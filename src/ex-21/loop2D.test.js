692b4a763fb79c89a4fd19e361f1ccc1d513cfcb
percegaroli <bruno.percegaroli@gmail.com>
2024-06-12 20:05:07 -0300

    fix test exercise-21

--- src/ex-21/loop2D.test.js
+++ src/ex-21/loop2D.test.js
@@ -6,13 +6,13 @@ describe('loop2D', () => {
         [1, 'A', 'A'],
         [2, 'A', 'A\nA A'],
         [5, 'A', 'A\nA A\nA A A\nA A A A\nA A A A A'],
-        [10, '0', '0\n0 0\n0 0 0\n0 0 0 0\n0 0 0 0 0\n0 0 0 0 0 0\n0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0']
+        [10, '0', '0\n0 0\n0 0 0\n0 0 0 0\n0 0 0 0 0\n0 0 0 0 0 0\n0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0'],
+        [0, 'B', '']
     ])('loop2D(%i, %s) => %s', (number, char, result) => {
         expect(loop2D(number, char)).toBe(result);
     })
 
     it.each([
-        [0, 'B'],
         [-20, 'C']
     ])('loop2D(%i, %s) => undefined', (number, char) => {
         expect(loop2D(number, char)).toBeUndefined();
