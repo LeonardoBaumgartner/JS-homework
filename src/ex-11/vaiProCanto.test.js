8991f1ec6140d96eac16e9a64f7774d6183d2aa6
percegaroli <bruno.percegaroli@gmail.com>
2024-06-12 19:08:46 -0300

    remove error test in exercise 11

--- src/ex-11/vaiProCanto.test.js
+++ src/ex-11/vaiProCanto.test.js
@@ -12,10 +12,4 @@ describe('vaiProCanto', () => {
     ])('vaiProCanto($name) => Vai pro canto $name!', (nome) => {
         expect(vaiProCanto(nome)).toBe(`Vai pro canto ${nome}!`)
     });
-
-    it('Deve retornar undefined caso o nome não seja uma string', () => {
-        expect(vaiProCanto(1)).toBe(undefined);
-        expect(vaiProCanto(false)).toBe(undefined);
-        expect(vaiProCanto(true)).toBe(undefined);
-    })
 })
